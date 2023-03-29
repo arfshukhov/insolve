fd659fbab6c8824836b2f81be20a12bf24103c65
Vladimir Shukhov <81926456+arfshukhov@users.noreply.github.com>
2023-03-30 03:27:22 +0500

    Add files via upload

--- logic.py
+++ logic.py
@@ -1,3 +1,39 @@
+import json
+import os
+
+
+from cryptography.fernet import Fernet
+
+
+class Cryptographer:
+    KEY = b'pbBf8hbN2YybfvP_ieCQmZf7ZDdg9934mY7bA_b4rUM='
+    fernet = Fernet(KEY)
+
+    @staticmethod
+    def encrypt(word, fernet=fernet):
+        return fernet.encrypt(word.encode())
+
+    @staticmethod
+    def decrypt(word, fernet=fernet):
+        print(word)
+        return fernet.decrypt(word).decode()
+
+
+class User:
+    def __init__(self, role, fname, lname):
+        self.role = role
+        self.fname = fname
+        self.lname = lname
+        self.__dict__ = {"role": self.role, "fname": self.fname, "lname": self.lname}
+
+    def __str__(self):
+        if self.role == "teacher":
+            role = "Учитель"
+        else:
+            role = "Ученик"
+        return f"{role}: {self.fname} {self.lname}"
+
+
 class Task:
     def __init__(self, text, answer, solution):
         self.text: str = text
@@ -21,3 +57,34 @@ class Editor:
 
     def remove_task(self, index):
         self.tokens["tasks"].pop(index)
+
+    def make_json(self):
+        with open(f"files/{self.tokens['title']}.json", "wb") as f:
+            jsn = Cryptographer.encrypt(json.dumps(self.tokens))
+            f.write((jsn))
+
+    def remove_json(self):
+        os.remove(f"files/{self.tokens['title']}.json")
+
+
+class Solution():
+    def __init__(self):
+        self.tokens = {
+            "title": ...,
+            "solutions": [],
+            "right_percent": ...,
+            "name": ...
+        }
+        self.data = None
+
+    def set_title(self, title):
+        self.tokens["title"] = title
+
+    def add_new_solution(self, text, solution, answer):
+        self.tokens["solutions"].append({"text": text, "solution": solution, "answer": answer})
+
+    def set_result(self, percent):
+        self.tokens["right_percent"] = percent
+
+    def set_name(self, user):
+        self.tokens["name"] = str(user)
--- main.py
+++ main.py
@@ -1,14 +1,72 @@
+import json
+import os
+import random
+from fileinput import filename
+from werkzeug.utils import secure_filename
 from flask import *
 from flask import Flask
+from logic import *
 import webbrowser
-from logic import Editor
 app = Flask(__name__, template_folder="templates")
 
+"""
+template of user data {"fname": "", "lname": "", "role": ""}
+"""
+
 editor = None
+user = None
+solution = None
+
+UPLOAD_FOLDER = os.path.join('tasks')
+ALLOWED_EXTENSIONS = ["json"]
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/insolve/', methods=['GET', 'POST'])
+def front_page():
+    jsn = None
+    with open("data/data.json", "r", encoding="utf-8") as f:
+        data = f.read()
+        print(f"data {data}")
+        jsn = json.loads(data)
+    if jsn["role"] == "" or jsn["fname"] == "" or jsn["lname"] == "":
+        return redirect("/insolve/regiester/")
+    else:
+        global user
+        user = str(User(jsn["role"], jsn["fname"], jsn["lname"]))
+        role = json.loads(data)["role"]
+        if role == "student":
+            ...
+        elif role == "teacher":
+            return redirect("/editor/")
+        else:
+            return redirect("/insolve/regiester/")
+
+
+@app.route('/insolve/regiester/')
+def regiester():
+    return render_template("regiester.html")
+
+
+@app.route('/insolve/regiester/done/', methods=['GET', 'POST'])
+def regiester_done():
+    role = request.form["role"].strip()
+    fname = request.form["fname"].strip()
+    lname = request.form["lname"].strip()
+    if role == "" or fname == "" or lname == "":
+        return redirect("/insolve/regiester/")
+    else:
+        with open("data/data.json", "w", encoding='utf-8') as f:
+            json.dump(User(role, fname, lname).__dict__, f, ensure_ascii=False)
+            f.close()
+        return redirect("/insolve/")
+
+
 
 
 @app.route('/editor/', methods=['GET', 'POST'])
 def start_editor():
+    global user
     start_action = """
     <div>
     <br>
@@ -20,12 +78,12 @@ def start_editor():
     </form>
     </div>
     """
-    return render_template("editor.html", action=start_action, tasks="")
+    return render_template("editor.html", user=user, action=start_action)
 
 
 @app.route('/editor/edit/', methods=['GET', 'POST'])
 def editor_main():
-    global editor
+    global editor, user
     if not editor:
         name = request.form["name"]
         editor = Editor(name)
@@ -33,11 +91,11 @@ def editor_main():
     else:
         print("editor is already exists")
     code = f"""
-    <div>
-    <input type="text" value="{editor.tokens.get("title")}" style="width:300px;"></input>
-    </div>
-"""
-    return render_template("editor_edit.html", action=code, tasks=editor.tokens["tasks"])
+        <div>
+        <input type="text" value="{editor.tokens.get("title")}" name="name" style="width:300px;"></input>
+        </div>
+    """
+    return render_template("editor_edit.html", user=user, action=code, tasks=editor.tokens["tasks"])
 
 
 @app.route('/editor/edit/new_task/', methods=['GET', 'POST'])
@@ -56,6 +114,99 @@ def remove_task(number):
     return redirect("/editor/edit/")
 
 
+@app.route('/editor/edit/finish/', methods=['GET', 'POST'])
+def finish():
+    global editor, user
+    editor.make_json()
+    fl = f"files/{editor.tokens['title']}.json"
+    return render_template("finish.html", user=user)
+
+
+@app.route("/editor/edit/finish/download/", methods=['GET', 'POST'])
+def download():
+    return send_file(f"files/{editor.tokens['title']}.json", as_attachment=True)
+
+@app.route('/overload/')
+def overload():
+    global editor
+    editor.remove_json()
+    editor = None
+    return redirect("/editor/")
+
+
+@app.route("/solver/", methods=['GET', 'POST'])
+def solver():
+    global user
+    return render_template("solver_start.html", user=user)
+
+
+@app.route("/solver/task/", methods=['GET', 'POST'])
+def solver_task():
+    global user, solution
+    code = ""
+    data = None
+    user = None
+    if request.method == 'POST':
+        file = request.files.get("file")
+        if file:
+            flname = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], flname))
+            with open(f"tasks/{flname}", "rb") as task:
+                text = Cryptographer.decrypt(task.read())
+                data = json.loads(text)
+                for number, task in enumerate(data["tasks"]):
+                    code += """<br>"""
+                    code += """<div class="task_widget">"""
+                    code += f"""<br><div><label>Задача {number+1}. {task["text"]}</label></div>"""
+                    if task["solution"] == "true":
+                        solution_name = "solution-"+str(number+1)
+                        code += f"""<div>Ваше решение: <textarea name="{solution_name}" style="min-height:100px;
+                        min-width:550px"></textarea></div>"""
+                    answer_name = "answer-"+str(number+1)
+                    code += f"""<div>Ответ: <input name="{answer_name}"></div>"""
+                    code += "<br></div>"
+                solution = Solution()
+                solution.data = data
+            os.remove(f"tasks/{flname}")
+        return render_template("solver_task.html", content=code, user=user, title=data["title"])
+
+
+@app.route("/solver/task/done/", methods=["GET", "POST"])
+def solver_done():
+    global solution, user
+    right_answers = []
+    total_answers = []
+    solution.set_title(solution.data["title"])
+    for idx, element in enumerate(solution.data["tasks"]):
+        text = element["text"]
+        if element["solution"] == "true":
+            solution_ = request.form[f"solution-{idx+1}"]
+        else:
+            solution_ = "none"
+        answer = request.form[f"answer-{idx+1}"]
+        solution.add_new_solution(text, solution_, answer)
+        if solution.data["tasks"][idx]["answer"] == answer:
+            total_answers.append(answer)
+            right_answers.append(answer)
+        else:
+            total_answers.append(answer)
+    right_percent = f"{len(right_answers)/len(total_answers)*100}%"
+    solution.set_result(right_percent)
+    solution.set_name(user)
+    return render_template("solver_done.html", user=user, percent=right_percent)
+
+
+@app.route("/solver/task/download/", methods=["GET", "POST"])
+def solution_download():
+    global solution
+    jsn = json.dumps(solution.tokens)
+    name = f"{random.randint(1, 10*10)}.json"
+    with open(f"files/{name}", "w", encoding="utf-8") as f:
+        f.write(jsn)
+        solution = None
+    return send_file(f"files/{name}", as_attachment=True)
+
+
 if __name__ == "__main__":
-    webbrowser.open("http://127.0.0.1:5000/editor/")
-    app.run(host='0.0.0.0')
+    webbrowser.open("http://127.0.0.1:5000/insolve/")
+    app.run(host='0.0.0.0', debug=True)
